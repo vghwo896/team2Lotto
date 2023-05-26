c0d613729b38077f0024bab385e76db3eabf5c0a
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 14:30:46 +0900

    당첨 번호 반복 오류 수정

--- team2Lotto/src/Main.java
+++ team2Lotto/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 	public static void main(String[] args) {	
 		LottoManager m = new LottoManager();
-		
+		m.gen.SelectNumber(m.getValue(1), 5, 45, 6, 12, 15, 44);
+		System.out.println(m.getValue(1));
+		m.winNumberGen();
+		m.gen.RandomGen(m.getValue(2));
 	}
 }
