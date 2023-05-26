93c4aed811bf0bf5d289867d66d7c5d5365b8df2
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 15:43:40 +0900

    자동 반자동 중간점검 콘솔출력

--- team2Lotto/src/GenNumber.java
+++ team2Lotto/src/GenNumber.java
@@ -2,19 +2,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
-public class GenNumber{
+public class GenNumber {
 
-	List<Integer> list= new ArrayList<>();
-	
+	List<Integer> list;
+
 	// 로또 매니저에 정보를 가져오기 위해서 로또 매니저 초기화
-	
+
 	public GenNumber() {
-		
+		list = new ArrayList<>();
 	}
-	
-	//맵의 getKey 메서드를 통해서 해당 value 의 로또를 랜덤으로 초기화 하는 메서드.
-	public int RandomGen(Lotto lotto){
+
+	public List<Integer> getList() {
+		return list;
+	}
+
+	public void setList(List<Integer> list) {
+		this.list = list;
+	}
+
+	// 맵의 getKey 메서드를 통해서 해당 value 의 로또를 랜덤으로 초기화 하는 메서드.
+	public int RandomGen(Lotto lotto) {
 		Random random = new Random();
 		int count = 0;
 		for (int i = 0; i < lotto.getNum().length; i++) {
@@ -29,60 +38,75 @@ public class GenNumber{
 		Arrays.sort(lotto.getNum());
 		System.out.println(lotto.toString());
 		System.out.println(count);
-        return count;
-    }
-	
+		return count;
+	}
+
 	// 클릭 액션
-    public void SelectNumber(int i){
-    	if(list.size()<6) {
-    	getList().add(i); 	
-    	}
-    }
-    
-    //선택 확정 버튼에 들어갈 액션.
-    
-    public int Confirmed(List<Integer> list,Lotto lotto) {
-    	list= this.list;
-    	Random random = new Random();
-    	int count = 0;
-    	for(int i = 0 ; i<list.size(); i++) {
-    	lotto.getNum()[i] = list.get(i);
-    	}
-    	
-    	for (int i = 0; i < lotto.getNum().length; i++) {
-			if(lotto.getNum()[i] == 0) {
-			lotto.getNum()[i] = (int)random.nextInt(45) + 1;
-			
-			for (int j = 0; j < i; j++) {
-					if ( lotto.getNum()[i] == lotto.getNum()[j]) {
-						lotto.getNum()[i] = (int)random.nextInt(45) + 1;
+	public void SelectNumber(int i) {
+		if (list.size() < 6) {
+			getList().add(i);
+		}
+	}
+
+	// 선택 확정 버튼에 들어갈 액션.
+
+	public int Confirmed(List<Integer> list, Lotto lotto) {
+		list = getList();
+		Random random = new Random();
+		int count = 0;
+		for (int i = 0; i < list.size(); i++) {
+			lotto.getNum()[i] = list.get(i);
+		}
+
+		for (int i = 0; i < lotto.getNum().length; i++) {
+			if (lotto.getNum()[i] == 0) {
+				lotto.getNum()[i] = (int) random.nextInt(45) + 1;
+
+				for (int j = 0; j < i; j++) {
+					if (lotto.getNum()[i] == lotto.getNum()[j]) {
+						lotto.getNum()[i] = (int) random.nextInt(45) + 1;
 					}
 				}
 			}
-			count++;	
+			count++;
 		}
-    	System.out.println(lotto.toString());
-    	return count;
-    }
-    	
-    
+		System.out.println(lotto.toString());
+		return count;
+	}
 
-    public void Retouch(){
-    	
-    }
-    
-    
-public static void main(String[] args) {
-	GenNumber gen=new GenNumber();
-	gen.SelectNumber(5);
-	gen.SelectNumber(7);
-	gen.SelectNumber(10);
-	
-	gen.Confirmed(gen.list, new Lotto());
-	
+	public void Retouch() {
+	}
+
+	public static void main(String[] args) {
+		GenNumber gen = new GenNumber();
+		Scanner scan = new Scanner(System.in);
+		for (int i = 0; i < 6; i++) {
+			System.out.println("1. 번호 선택(수동)    2. 자동");
+			int choice = scan.nextInt();
+			
+			switch (choice) {
+			case 1:
+				System.out.println("수동 선택\n번호 입력");
+				int num = scan.nextInt();
+				for (int j = 0; j < gen.list.size(); j++) {
+					if(gen.list.contains(num)) {
+						System.out.println("true");
+						gen.SelectNumber(num);
+					}else {
+						System.out.println("다시입력");
+					}
+				}
+				break;
+			case 2:
+				gen.Confirmed(gen.list, new Lotto());
+				break;
+			case 3:
+				
+			default:
+				break;
+			}
+			
+		}
+	}
+
 }
-    
-    
-}
-
-
