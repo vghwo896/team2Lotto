c347a970fa0ce17db5a4dfba88d4c094016f7bf8
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 12:04:02 +0900

    선택을 눌렀을때 숫자가 랜덤으로 바뀌는 메서드

--- team2Lotto/src/GenNumber.java
+++ team2Lotto/src/GenNumber.java
@@ -1,18 +1,35 @@
-public class GenNumber extends LottoManager {
+import java.util.Arrays;
+import java.util.Random;
 
-    
-	LottoManager LM ;
+public class GenNumber{
+
+  
 	
 	// 로또 매니저에 정보를 가져오기 위해서 로또 매니저 초기화
 	
 	public GenNumber() {
-		LM = new LottoManager();
+		
 	}
 	
 	//맵의 getKey 메서드를 통해서 해당 value 의 로또를 랜덤으로 초기화 하는 메서드.
 	public int RandomGen(Lotto lotto){
-//		lotto.setNum(i);
-        return 0;
+		Random random = new Random();
+		int count = 0;
+		for (int i = 0; i < lotto.getNum().length; i++) {
+			lotto.getNum()[i] = random.nextInt(45) + 1;
+			for (int j = 0; j < i; j++) {
+				if (lotto.getNum()[j] == lotto.getNum()[i]) {
+					lotto.getNum()[i] = random.nextInt(45) + 1;
+//					count++;
+//					break;
+				}
+			}
+			count++;
+		}
+		Arrays.sort(lotto.getNum());
+		System.out.println(lotto.toString());
+		System.out.println(count);
+        return count;
     }
 
     public void SelectNumber(){
@@ -20,6 +37,4 @@ public class GenNumber extends LottoManager {
 
     public void Retouch(){
     }
-    
-    
 }
