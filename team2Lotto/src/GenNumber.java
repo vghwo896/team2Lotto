62982d65c216a8d3c5ad2d0f4eb94f210de936aa
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 11:03:07 +0900

    getKey 메서드를 통해서 맵의 해당 키의 밸류에 lotto 를 가져와서 랜덤 숫자 생성

--- team2Lotto/src/GenNumber.java
+++ team2Lotto/src/GenNumber.java
@@ -1,9 +1,17 @@
 public class GenNumber extends LottoManager {
 
     
+	LottoManager LM ;
 	
+	// 로또 매니저에 정보를 가져오기 위해서 로또 매니저 초기화
 	
-	public int RandomGen(){
+	public GenNumber() {
+		LM = new LottoManager();
+	}
+	
+	//맵의 getKey 메서드를 통해서 해당 value 의 로또를 랜덤으로 초기화 하는 메서드.
+	public int RandomGen(Lotto lotto){
+//		lotto.setNum(i);
         return 0;
     }
 
