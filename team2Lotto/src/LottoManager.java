96a3b935a78d594a523679a114d4f02cdca585a0
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 10:56:12 +0900

    당첨 번호 메소드 주석 추가

--- team2Lotto/src/LottoManager.java
+++ team2Lotto/src/LottoManager.java
@@ -10,6 +10,7 @@ public class LottoManager {
 //		lottoMap.put(1, new Lotto());
 //	}
 
+	// 당첨 번호 7자리를 랜덤 값으로 입력을 받아 winNumber에 넣어준다
 	public void winNumberGen() {
 		winNumber = new int[7];
 		Random random = new Random();
