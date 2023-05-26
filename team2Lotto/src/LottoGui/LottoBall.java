8d62d1f2ddf6bf8e8feded585ccfa4c44dabd0c7
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-26 17:15:35 +0900

    판넬 3개로 분리 판넬 위치 설정 setText로 각각 공들 이름 설정

--- team2Lotto/src/LottoGui/LottoBall.java
+++ team2Lotto/src/LottoGui/LottoBall.java
@@ -1,63 +1,70 @@
 package LottoGui;
 
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-
-import javax.swing.JSplitPane;
 
 class ImageFrame extends JFrame {
 	private JPanel panel;
 	private JLabel lbl;
-	int count;
 	boolean condition = false; // false가 클릭 해제 상태
 	ImageIcon ball = new ImageIcon("번호(미선택).png");
 	ImageIcon selectedball = new ImageIcon("번호(선택).png");
 
-	public ImageFrame() {
-		JPanel pnl = new JPanel();
-		JPanel pnl2 = new JPanel();
+	public void paintComponent(Graphics g) {
+		super.paintComponents(g);
+		g.setColor(Color.RED);
+		g.drawArc(20, 100, 80, 80, 90, 270);
+	}
 
+	public ImageFrame() {
+		JPanel pnlGuide = new JPanel();
+		pnlGuide.setBounds(0, 0, 212, 768);
+		JPanel pnlLeft = new JPanel();
+		pnlLeft.setBounds(212, 0, 300, 768);
+		JPanel pnlRight = new JPanel();
+		pnlRight.setBounds(512, 0, 512, 768);
 		JSplitPane jsp = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
 
-		JLabel[] lbl = new JLabel[45]; // 버튼
+		JLabel[] lbl = new JLabel[47]; // 버튼
 
-		for (int i = 0; i < 45; i++) {
-			lbl[i] = new JLabel(ball);// 버튼 초기화
-			lbl[i].setVisible(true);// 보이게
-			lbl[i].setBorder(BorderFactory.createEmptyBorder());
-			pnl.add(lbl[i]);// 프레임에 버튼 추가
+		MouseAdapter click = new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				JLabel oooo = (JLabel) e.getSource();
 
-			lbl[i].addMouseListener(new MouseAdapter() {// 마우스 클릭 이벤트
+				if (condition == false) {
+					// System.out.println("클릭 시");
+					oooo.setIcon(selectedball);
 
-				@Override
-				public void mouseClicked(MouseEvent e) {
-					JLabel oooo = (JLabel) e.getSource();
-
-					if (count<7 && condition == false) {
-						// System.out.println("클릭 시");
-						oooo.setIcon(selectedball);
-						condition = true;
-						count++;
-					}
-//							System.out.println("선택된거 클릭 시");
-					else {
-						oooo.setIcon(ball);
-						condition = false;
-					}
+					condition = true;
+					System.out.println(oooo.getText());
 
 				}
+//							System.out.println("선택된거 클릭 시");
+				else {
+					oooo.setIcon(ball);
+					condition = false;
+				}
 
-			});
+			}
 
+		};
+
+		for (int i = 1; i < 46; i++) {
+			lbl[i] = new JLabel(ball);// 버튼 초기화
+			lbl[i].setText(""+i);
+			lbl[i].setVisible(true);// 보이게
+			lbl[i].setBorder(BorderFactory.createEmptyBorder());
+			pnlLeft.add(lbl[i]);// 프레임에 버튼 추가
+
+			lbl[i].addMouseListener(click);
+			
 		}
-		add(pnl);
+		add(pnlGuide);
+		add(pnlLeft);
+		add(pnlRight);
 
 		setSize(1024, 768);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -68,7 +75,12 @@ class ImageFrame extends JFrame {
 
 public class LottoBall {
 	public static void main(String[] args) {
-		new ImageFrame();
+		ImageFrame imageFrame = new ImageFrame();
+		imageFrame.getContentPane().setLayout(null);
+		
+		JButton Select = new JButton("확정");
+		Select.setBounds(319, 454, 97, 23);
+		imageFrame.getContentPane().add(Select);
 
 	}
 }
